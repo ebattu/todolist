b6a977c768d047bbf745c103a23bf61808bb087a
eswarbattu <60275260+eswarbattu@users.noreply.github.com>
2020-01-24 15:21:00 -0600

    Add files via upload

---
+++ To_do_JS.js
@@ -0,0 +1,12 @@
+var app = angular.module('myapp',[])
+app.controller("ctrl",['$scope',function($scope){
+    $scope.tasks = [];
+    $scope.addTask = function(){
+        $scope.tasks.push($scope.taskname);
+        $scope.taskname='';
+    }
+    $scope.removeTask = function(task){
+        var index = $scope.tasks.indexOf(task);
+        $scope.tasks.splice(index,1);
+    }
+}])
